6def60d3881b7ba4c486b30812becb7223db8b18
Faezragore <30468128+Faezragore@users.noreply.github.com>
2019-12-26 20:55:07 +0300

    Update and rename secret_number.py to secret_string.py

--- secret_string.py
+++ secret_string.py
@@ -1,11 +1,11 @@
 from django.utils.crypto import get_random_string
 
-def get_a_large_random_number():
+def get_a_large_random_string():
     chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
     print(get_random_string(50, chars))
 
 def main():
-    get_a_large_random_number()
+    get_a_large_random_string()
     
 if __name__ == '__main__':
     main()
