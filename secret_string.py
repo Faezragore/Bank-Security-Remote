f5169e03c20b39dd9498cabd8417675654ba2207
Faezragore <30468128+Faezragore@users.noreply.github.com>
2019-12-27 22:37:38 +0300

    update

--- secret_string.py
+++ secret_string.py
@@ -3,7 +3,7 @@ from django.utils.crypto import get_random_string
 
 def main():
     chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
-    return get_random_string(50, chars)
+    print(get_random_string(50, chars))
 
 
 if __name__ == '__main__':
