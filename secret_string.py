a2a833830d57349b5c6a9309309d906a791d8125
Faezragore <30468128+Faezragore@users.noreply.github.com>
2019-12-27 22:41:16 +0300

    update: removal_def print

--- secret_string.py
+++ secret_string.py
@@ -3,8 +3,8 @@ from django.utils.crypto import get_random_string
 
 def main():
     chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
-    print(get_random_string(50, chars))
+    return get_random_string(50, chars)
 
 
 if __name__ == '__main__':
-    main()
+    print(main())
